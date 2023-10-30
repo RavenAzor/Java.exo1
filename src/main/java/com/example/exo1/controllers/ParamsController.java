72db26bb3a6905ca248ea12e78ff0fcb5a1d13d5
justine <justine.bels04@gmail.com>
2023-10-30 15:28:27 +0100

    new commit v2

--- src/main/java/com/example/exo1/controllers/ParamsController.java
+++ src/main/java/com/example/exo1/controllers/ParamsController.java
@@ -10,58 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/api/v1/base")
+@RequestMapping(value = "/base")
 public class ParamsController {
 
     @RequestMapping(value = "1-param")
 //    @ResponseBody
-    public String unParam(Model model){
+    public String unParam(Model model) {
         model.addAttribute("firstperson", "Harley");
         return "param1";
     }
 
     @RequestMapping(value = "2-params")
 //    @ResponseBody
-    public String deuxParam(Model model){
+    public String deuxParam(Model model) {
         model.addAttribute("firstperson", "Harley");
         model.addAttribute("secondperson", "Ivy");
         return "param2";
     }
 
+    @RequestMapping(value = "coll-params")
 //    @ResponseBody
-//    @RequestMapping(value = "/params")
-//    public String personName(Model model) {
-//        List<String> persons = List.of("John Dupont", "Maria Martez", "Chloé Smith");
-////        List<String> persons = List.of();
-//        model.addAttribute("persons", persons);
-//        return "allPersons";
-//    }
-
-    @GetMapping(value = "params")
-    @ResponseBody
-    public List<String> personJson(){
-        return List.of("John Dupont", "Maria Martez", "Chloé Smith");
+    public String personName(Model model){
+        List<String> persons = List.of("John Dupont", "Maria Martez", "Chloé Smith");
+//        List<String> persons = List.of();
+        model.addAttribute("persons", persons);
+        return "allPersons";
     }
 }
-
-
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping(value = "/api/v1/base")
-//public class ParamsController {
-//
-//    @GetMapping
-//    public String param1(){
-//        return "param1";
-//    }
-//
-//    //    @RequestMapping(value ="/persons")
-//    @GetMapping(value = "coll-params")
-//    @ResponseBody
-//    public List<String> personJson(){
-//        return List.of("John Dupont", "Maria Martez", "Chloé Smith");
-//    }
-//}
--- src/main/java/com/example/exo1/controllers/ParamsRestController.java
+++ src/main/java/com/example/exo1/controllers/ParamsRestController.java
@@ -1,2 +1,31 @@
-package com.example.exo1.controllers;public class ParamsRestController {
+package com.example.exo1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1/base")
+public class ParamsRestController {
+
+    @GetMapping(value = "1-param")
+    @ResponseBody
+    public List<String> unParam() {
+        return List.of("Harley");
+    }
+
+    @GetMapping(value = "2-params")
+    @ResponseBody
+    public List<String> deuxParam() {
+        return List.of("Harley", "Ivy");
+    }
+
+    @GetMapping(value = "params")
+    @ResponseBody
+    public List<String> personJson(){
+        return List.of("John Dupont", "Maria Martez", "Chloé Smith");
+    }
 }
