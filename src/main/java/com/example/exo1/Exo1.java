85e7b8baf631d8acf60aa7fcd9edcdbca7239290
justine <justine.bels04@gmail.com>
2023-10-30 15:06:55 +0100

    java exo1 commit

---
+++ src/main/java/com/example/exo1/Exo1.java
@@ -0,0 +1,15 @@
+package com.example.exo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exo1{
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(com.example.exo1.Exo1.class, args);
+    }
+
+}
+
