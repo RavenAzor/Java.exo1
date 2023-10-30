98bd20051e19cc5169f0136b1078d41255a04a3f
justine <justine.bels04@gmail.com>
2023-10-30 15:47:14 +0100

    new commit v3

--- src/main/java/com/example/exo1/controllers/ParamsController.java
+++ src/main/java/com/example/exo1/controllers/ParamsController.java
@@ -1,11 +1,8 @@
 package com.example.exo1.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
